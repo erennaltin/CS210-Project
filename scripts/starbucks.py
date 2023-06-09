fac6fb54816098aba32acc07babc7b9ce356c05e
Eren ALTIN <eren@pragmasoft.com.tr>
2023-06-09 23:42:08 +0300

    starbucks data

---
+++ scripts/starbucks.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+cities = {}
+
+try: 
+  r = requests.get('https://www.shaya.com.tr/tr/magazalarimiz/?topZone=32')
+  soup = bs(r.content, 'html.parser')
+  data = soup.find('table', {'class':"generalTable"})
+  data = data.find_all('tr')[1:]
+  for i in data:
+    nodes = i.find_all('td')
+    city = nodes[2].text
+
+    if city in cities:
+      cities[city] += 1
+    else:
+      cities[city] = 1
+
+except:
+  print("An exception occurred")
+
+# sort cities according to their values
+cities = dict(sorted(cities.items(), key=lambda item: item[1], reverse=True))
+
+for city in cities:
+  with open('./datasets/starbucks.txt', 'a') as f:
+    f.write(city + ' ' + str(cities[city]) + '\n')
