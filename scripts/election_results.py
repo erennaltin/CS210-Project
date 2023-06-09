b1ef40566dca09d356c7608cf32ffd45aca4485f
Fexyler <batuhanisildak@sabanciuniv.edu>
2023-06-09 22:13:58 +0300

    fixed the path

--- scripts/election_results.py
+++ scripts/election_results.py
@@ -8,11 +8,11 @@ This script will return a json object that contains the following data:
     "Adiyaman": "CHP" or "AKP" or "MHP" or "IYI" or "HDP" or "SP" or "DP" or "BBP" or "VP" or "TKP" or "Diger",
     ...
 }
+
 """
 
 import json
 import requests
-import pandas as pd
 from requests.exceptions import HTTPError
 
 url = "https://secim2023.cnnturk.com/api/home/"
@@ -35,5 +35,5 @@ else:
         province_name = item.get('regionName')
         provinces[province_name] = winner_name
 
-    open('election_results.json', 'w', encoding='utf-8').write(
+    open('../datasets/election_results.json', 'w', encoding='utf-8').write(
         json.dumps(provinces, indent=4, sort_keys=True, ensure_ascii=False))
